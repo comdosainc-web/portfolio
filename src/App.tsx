a2528f3e16b73bdd48827dd824697190440ab574
comdosainc-web <comdosainc@gmail.com>
2026-06-08 10:36:24 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
+import Home from "./home";
+
 export default function App() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>컴도사 포트폴리오 사이트</h1>
-      <p>수리 문의 및 포트폴리오 준비 중입니다.</p>
-    </div>
-  );
+  return <Home />;
 }
